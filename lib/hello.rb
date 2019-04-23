6d4da6052b3ffff1d90561e34fc163935120b1b8
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 01:08:16 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,8 @@
 def hello_t(names)
   names.each do |name|
-    binding.pry
     yield name
   end
 end
 
-hello_t(names) { |name| puts "Hello, #{name}." if name.begins_with("T") }
+hello_t(names) { |name| puts "Hello, #{name}." if name.start_with?("T") }
 
